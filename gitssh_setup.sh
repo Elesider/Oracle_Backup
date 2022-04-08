14cdd4d10f132d8cc8fb6a5e0524f6c4f63ea797
Ubuntu <giantenormouscanada@gmail.com>
2022-04-08 01:02:37 +0000

    trying apri 7

--- gitssh_setup.sh
+++ gitssh_setup.sh
@@ -2,6 +2,8 @@
 eval "$(ssh-agent -s)"
 # -k has to be added Unknow bug. I have to run the code line by line. Cannot run as a bashscript
 ssh-add -k  ~/.ssh/oracle_setup
+# hold on it to avoid optimization
  sleep 5
+ echo "Input niz1 computer password"
 ssh -T git@github.com
 
