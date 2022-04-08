836a3b2e9ec8c2db8692bb744d46bf648a452f62
Ubuntu <giantenormouscanada@gmail.com>
2022-04-08 00:06:22 +0000

    april 7th

--- gitssh_setup.sh
+++ gitssh_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/oracle_setup
+ssh-add -k  ~/.ssh/oracle_setup
 ssh -T git@github.com
-git remote add origin git@github.com:Elesider/Oracle_Backup.git
 
