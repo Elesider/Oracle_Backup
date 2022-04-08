7db7c4f1cd1c46a24cd9ec9e8b50b86be6a3f727
Ubuntu <giantenormouscanada@gmail.com>
2022-04-08 00:58:45 +0000

    april 7th last v2

--- gitssh_setup.sh
+++ gitssh_setup.sh
@@ -2,5 +2,6 @@
 eval "$(ssh-agent -s)"
 # -k has to be added Unknow bug. I have to run the code line by line. Cannot run as a bashscript
 ssh-add -k  ~/.ssh/oracle_setup
+ sleep 5
 ssh -T git@github.com
 
