59463daa6cd05d5b899c6f0a9a689f70c626af5a
Ubuntu <giantenormouscanada@gmail.com>
2022-12-18 07:37:04 +0000

    Test

--- backup.sh
+++ backup.sh
@@ -13,3 +13,6 @@ trash-put .profile
 ln ~/.profile
 trash-put docker-compose.yml  
 ln ~/dcontainers/WordE/docker-compose.yml
+# Actually no need of trash-put for all existing document.
+# once use the ln, the document here are actually the mirror of the exisitng one.
+# It will change once chnaged it in the ~ Home directory.
